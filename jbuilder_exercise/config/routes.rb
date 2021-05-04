bf009a57c43324aab045fa6475f28c10cb188f6b
Chris Joo <chrisj1225@gmail.com>
2021-05-04 15:42:24 -0400

    Finish Parties jbuilders & bonus

--- jbuilder_exercise/config/routes.rb
+++ jbuilder_exercise/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       resources :gifts, only: [:index]
     end 
     resources :gifts, only: [:show]
+    resources :parties, only: [:show, :index]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
